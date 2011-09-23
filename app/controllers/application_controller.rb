fffcdb49b8c38ffc2bb0f5803c2b9f5912b77d69
Bryan Bibat <bry@bryanbibat.net>
2011-09-24 03:55:22 +0800

    finishing touches to devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :authenticate_user!
+
+  private
+  def after_sign_out_path_for(resource_or_scope)
+    scope = Devise::Mapping.find_scope!(resource_or_scope)
+    send(:"new_#{scope}_session_path")
+  end
 end
